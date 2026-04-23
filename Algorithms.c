722b6e10421eb250777f2326bf177b6956e9aab2
Oliver-Kaka <oliverkaka715@gmail.com>
2026-04-23 09:15:39 +0300

    Adding Linear search algorithm

--- Algorithms.c
+++ Algorithms.c
@@ -2,7 +2,18 @@
 #include <stdlib.h>
 #include <time.h>
 
+int linearSearch(int arr[], int size, int key);
+
 int main(){
 
     return 0;
+}
+
+int linearSearch(int arr[], int size, int key) {
+    for(int i = 0; i < size; i++) {
+        if(arr[i] == key) {
+            return i; // return index where key is found
+        }
+    }
+    return -1; // key not found
 }
