eec3c3a7197d0c8085789fef1fa16d104e40fabb
Oliver-Kaka <oliverkaka715@gmail.com>
2026-04-23 09:18:11 +0300

    Adding Binary search algorithm

--- Algorithms.c
+++ Algorithms.c
@@ -3,6 +3,7 @@
 #include <time.h>
 
 int linearSearch(int arr[], int size, int key);
+int binarySearch(int arr[], int left, int right, int key);
 
 int main(){
 
@@ -16,4 +17,21 @@ int linearSearch(int arr[], int size, int key) {
         }
     }
     return -1; // key not found
+}
+
+int binarySearch(int arr[], int left, int right, int key) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == key) {
+            return mid; // element found
+        }
+        else if (arr[mid] < key) {
+            left = mid + 1; // search right half
+        }
+        else {
+            right = mid - 1; // search left half
+        }
+    }
+    return -1; // element not found
 }
